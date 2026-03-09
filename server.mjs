02825989f232fcc65ff43a0a26af11a1377ca1d3
Aakash-shrestha <akash.stha444@icloud.com>
2026-03-09 22:43:48 +0545

    updated server to handle change in nodes

--- server.mjs
+++ server.mjs
@@ -10,20 +10,40 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 
-  socket.on("join-note", (noteId) => {
-    socket.join(noteId);
-    console.log(`Socket ${socket.id} joined note: ${noteId}`);
+  // Join a board room (authorId is the room)
+  socket.on("join-board", (boardId) => {
+    socket.join(boardId);
+    console.log(`Socket ${socket.id} joined board: ${boardId}`);
   });
 
-  socket.on("leave-note", (noteId) => {
-    socket.leave(noteId);
-    console.log(`Socket ${socket.id} left note: ${noteId}`);
+  socket.on("leave-board", (boardId) => {
+    socket.leave(boardId);
+    console.log(`Socket ${socket.id} left board: ${boardId}`);
   });
 
-  socket.on("content-change", (data) => {
-    socket.to(data.noteId).emit("content-update", {
-      content: data.content,
-    });
+  // Node was moved (position changed)
+  socket.on("node-moved", (data) => {
+    socket.to(data.boardId).emit("node-moved", data);
+  });
+
+  // Node was added
+  socket.on("node-added", (data) => {
+    socket.to(data.boardId).emit("node-added", data);
+  });
+
+  // Node content was edited
+  socket.on("node-edited", (data) => {
+    socket.to(data.boardId).emit("node-edited", data);
+  });
+
+  // Edge was added
+  socket.on("edge-added", (data) => {
+    socket.to(data.boardId).emit("edge-added", data);
+  });
+
+  // Edge was removed
+  socket.on("edge-removed", (data) => {
+    socket.to(data.boardId).emit("edge-removed", data);
   });
 
   socket.on("disconnect", () => {
