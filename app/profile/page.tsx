7c932faef40aa6ecd8f7f26de376b06d65483a99
Aakash-shrestha <akash.stha444@icloud.com>
2026-03-13 23:39:15 +0545

    added profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,84 @@
+import { prisma } from "@/lib/prisma";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function ProfilePage() {
+  const cookieStore = await cookies();
+  const userId = cookieStore.get("userId")?.value;
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  const user = prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      name: true,
+      email: true,
+      password: true,
+      profilePicture: true,
+    },
+  });
+
+  //friends stuff
+  // people i share my board with
+  const myBoardShares = await prisma.boardShare.findMany({
+    where: {
+      board: { ownerId: userId },
+    },
+    include: {
+      sharedWith: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          profilePicture: true,
+        },
+      },
+    },
+  });
+
+  const sharedWithMe = await prisma.boardShare.findMany({
+    where: { sharedWithId: userId },
+    include: {
+      board: {
+        include: {
+          owner: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              profilePicture: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const friendsMap = new Map<
+    string,
+    { id: string; name: string; email: string; profilePicture: string | null }
+  >();
+
+  for (const share of myBoardShares) {
+    friendsMap.set(share.sharedWith.id, share.sharedWith);
+  }
+  for (const share of sharedWithMe) {
+    friendsMap.set(share.board.owner.id, share.board.owner);
+  }
+
+  const friends = Array.from(friendsMap.values());
+  console.log("Friends:", friends);
+  return (
+    <ProfileClient
+      user={{
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        profilePicture: user.profilePicture,
+      }}
+      friends={friends}
+    />
+  );
+}
